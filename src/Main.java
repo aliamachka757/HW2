1211fec97eed9ffbfc5a1fe53e86d3d0aa91cd09
aliamachka757 <117412506+aliamachka757@users.noreply.github.com>
2022-11-10 07:14:10 -0800

    Homework part 3

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,45 @@
 public class Main {
     public static void main(String[] args) {
-       //Problem 4
-       System.out.println("Problem 4");
+       //Problem 6
+       System.out.println("Problem 6");
 
-       var friend= 19;
-       System.out.println(friend);
-       friend = friend /7;
-       System.out.println(friend);
-       friend = friend * 2;
-       System.out.println(friend);
+       var boxer = 78.2;
+       System.out.println(boxer);
+       var boxer2 =82.7;
+       System.out.println(boxer2);
+       var boxerWeight = boxer + boxer2;
+       System.out.println("combine weight of two boxers is "  +boxerWeight+  "kg'" );
 
-
-
-       //problem 5
-       System.out.println("Problem5");
-       var frog = 3.5;
-       System.out.println(frog);
-       frog = frog * 10;
-       System.out.println(frog);
-       frog = frog / 3.5;
-       System.out.println(frog);
-       frog = frog +4;
-       System.out.println(frog);
+       var odds = boxer2 - boxer;
+       System.out.println("one boxer bigger then other on " +odds+ "kg");
 
 
 
+       //problem 7
+       System.out.println("Problem 7");
+       var weightDifference = boxer2 - boxer;
+       System.out.println("First weight differance is: "+weightDifference+ "kg");
+       var weight2 = boxer2 % boxer;
+       System.out.println("Second Weight differences is:"  +weight2+ "kg");
 
 
+       //problem 8
+       System.out.println("Problem 8");
+       var allTogetherTime = 640;
+       System.out.println(allTogetherTime);
+       var personalTime  = 8;
+       System.out.println(personalTime);
+       var workers = 640 / 8;
+       System.out.println("all Together in company working "  +workers+  "people ");
+
+       var workers2 = 80;
+       System.out.println(workers2);
+       var workers3 = 94;
+       System.out.println(workers3);
+       var newWorkers = workers2 + workers3;
+       System.out.println(newWorkers);
+       var newHours = newWorkers * personalTime;
+       System.out.println("If in this company working " +newWorkers+ "workers , workers  will work " +newHours+ "amount of hours  ");
 
     }
 }
